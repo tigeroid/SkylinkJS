f421a09cc1c99d494223f7a9ef5b6506f2f29190
Leticia Choo <leticia.choo@temasys.com.sg>
2015-01-28 14:00:31 +0800

    ESS-92 #comment Updates on DataChannel class

--- source/DataChannel.js
+++ source/DataChannel.js
@@ -1,12 +1,177 @@
+/**
+ * Handles the DataChannel connections.
+ * @class DataChannel
+ * @for Skylink
+ * @since 0.6.0
+ */
 function DataChannel(channel, config, listener) {
   'use strict';
 
   // Reference of instance
   var com = this;
 
-  com.id = config.id;
+  /**
+   * The datachannel label.
+   * @attribute id
+   * @type String
+   * @private
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.id = channel.label || Date.UTC();
   
-  if (!channel) {
-    throw Error('DataChannel missing');
+  /**
+   * The type of datachannel
+   * @attribute type
+   * @type String
+   * @private
+   * @for DataMessage
+   * @since 0.6.0
+   */
+  com.type = 'message';
+
+  /**
+   * The peer the datachannel is linked to.
+   * @attribute peerId
+   * @type String
+   * @private
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.peerId = config.peerId;
+
+  /**
+   * The DataChannel object.
+   * @attribute RTCDataChannel
+   * @type Object
+   * @private
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.RTCDataChannel = null;
+  
+  /**
+   * Binds events to RTCDataChannel object.
+   * @method bind
+   * @trigger StreamJoined, mediaAccessRequired
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.bind = function (bindChannel) {
+    // Prevent re-trigger
+    if (bindChannel.readyState !== 'open') {
+      bindChannel.onopen = function () {
+        com.onOpen(bindChannel);
+      };
+    
+    } else {
+      com.onOpen(bindChannel);
+    }
+    
+    bindChannel.onerror = function(error) {
+      com.onError(bindChannel, error);
+    };
+
+    // NOTE: Older firefox might close the DataChannel earlier 
+    bindChannel.onclose = function() {
+      com.onClose(bindChannel);
+    };
+
+    bindChannel.onmessage = function(event) {
+      com.onMessage(bindChannel, event.data);
+    };
+    
+    com.RTCDataChannel = bindChannel;
+
+    fn.runSync(function () {
+      listener('datachannel:start', {
+        id: com.id,
+        peerId: com.peerId
+      });
+    });
+  };
+  
+  /**
+   * Handles the event when DataChannel is opened.
+   * @method onOpen
+   * @trigger peerJoined, mediaAccessRequired
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.onOpen = function (bindChannel) {
+    listener('datachannel:connect', {
+      id: com.id,
+      peerId: com.peerId
+    });
+  };
+  
+  /**
+   * Handles the event when DataChannel is closed.
+   * @method onClose
+   * @trigger peerJoined, mediaAccessRequired
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.onClose = function (bindChannel) {
+    listener('datachannel:disconnect', {
+      id: com.id,
+      peerId: com.peerId
+    });
+  };
+  
+  /**
+   * Handles the event when DataChannel has an exception.
+   * @method onClose
+   * @trigger peerJoined, mediaAccessRequired
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.onError = function (bindChannel, error) {
+    listener('datachannel:error', {
+      id: com.id,
+      peerId: com.peerId,
+      error: error
+    });
+  };
+  
+  /**
+   * Handles the event when DataChannel has a message received.
+   * @method onMessage
+   * @trigger peerJoined, mediaAccessRequired
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.onMessage = function (bindChannel, data) {
+    listener('datachannel:error', {
+      id: com.id,
+      peerId: com.peerId,
+      data: data
+    });
+  };
+  
+  /**
+   * Sends data over the datachannel.
+   * @method send
+   * @param {JSON|String} data The data to send.
+   * @trigger peerJoined, mediaAccessRequired
+   * @for DataChannel
+   * @since 0.6.0
+   */
+  com.send = function (data) {
+    var sendingData = data;
+  
+    if (typeof data === 'object') {
+      sendingData = JSON.stringify(data);
+    }
+  
+    fn.isSafe(function () {
+      com.RTCDataChannel.send(sendingData);
+    });
+  };
+
+  if (fn.isEmpty(channel)) {
+    throw new Error('Provided parameter channel is invalid.');
   }
+
+  com.bind(channel);
 }
