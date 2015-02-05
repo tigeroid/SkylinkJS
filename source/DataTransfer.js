9a299bca55b011643c669930d0108e34b4c4d692
Thanh Nguyen <tienthanh8490@gmail.com>
2015-02-05 12:58:48 +0800

    ESS-94 #comment Data transfer handler

--- source/DataTransfer.js
+++ source/DataTransfer.js
@@ -1,177 +1,64 @@
-function DataTransfer(channel, config, listener) {
+function DataTransfer(channel, peerId, listener) {
   'use strict';
 
-  // Reference of instance
   var com = this;
-
-  /**
-   * The connection id.
-   * @attribute id
-   * @type String
-   * @private
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.id = config.id || null;
   
-  /**
-   * The peer id.
-   * @attribute peerId
-   * @type String
-   * @private
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.peerId = config.peerId || null;
-  
-  /**
-   * The peer type.
-   * @attribute type
-   * @type String
-   * @private
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.type = config.type || 'stream';
+  com.peerId = peerId;
 
-  /**
-   * The PeerConnection constraints.
-   * @attribute constraints
-   * @type String
-   * @private
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.constraints = config.constraints;
+  com.channel = channel;
 
-  /**
-   * The PeerConnection configuration.
-   * @attribute config
-   * @type String
-   * @private
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.config = config.config || {
-    optional: [{
-      DtlsSrtpKeyAgreement: true
-    }]
+  com._DC_PROTOCOL_TYPE = {
+    WRQ: 'WRQ',
+    ACK: 'ACK',
+    ERROR: 'ERROR',
+    CANCEL: 'CANCEL',
+    MESSAGE: 'MESSAGE'
   };
   
-  /**
-   * The list of DataChannels connected to Peer.
-   * @attribute datachannels
-   * @param {DataChannel} main The main DataChannel for sending chats.
-   * @param {JSON} transfers The list of datachannel(s) for transfers.
-   * @param {DataChannel} [transfers.n=*] The DataChannel for sending data.
-   * @type Stream
-   * @private
-   * @for Peer
-   * @since 0.6.0
-   */
-  com.datachannels = {
-    main: null,
-    transfers: {}
-  };
+  com.dataHandler = function(dataString){
+    if (typeof dataString === 'string'){
+      var data = {};
+      try{
+        data = JSON.parse(dataString);
+      }
+      catch(error){
+        listener('datatransfer:binary',{
+          id: com.id,
+          peerId: com.peerId,
+          data: dataString
+        });
+        com.DATAProtocolHandler(dataString);
+        return;
+      }
 
-  /**
-   * The stream object.
-   * @attribute stream
-   * @type Stream
-   * @private
-   * @for Peer
-   * @since 0.6.0
-   */
-  com.stream = null;
+      listener('datatransfer:generic',{
+        peerId: com.peerId,
+        data: data
+      });
 
-  /**
-   * The PeerConnection object.
-   * @attribute RTCPeerConnection
-   * @type JSON
-   * @private
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.RTCPeerConnection = null;
-
-  /**
-   * Binds events to RTCPeerConnection object.
-   * @method bind
-   * @trigger StreamJoined, mediaAccessRequired
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.bind = function (bindPeer) {
-    bindPeer.queueCandidate = [];
-    bindPeer.iceConnectionFiredStates = [];
-    bindPeer.newSignalingState = 'new';
-    bindPeer.newIceConnectionState = 'checking';
-
-    bindPeer.ondatachannel = function (event) {
-      //var dc = event.channel || event;
-      com.onDataChannel(event);
-    };
-
-    bindPeer.onaddstream = function (event) {
-      com.onAddStream(event);
-    };
-
-    bindPeer.onicecandidate = function (event) {
-      com.onIceCandidate(event, bindPeer);
-    };
-
-    bindPeer.oniceconnectionstatechange = function (event) {
-      ICE.parseIceConnectionState(bindPeer);
-    };
-
-    bindPeer.oniceconnectionnewstatechange = function (event) {
-      com.onIceConnectionStateChange(bindPeer);
-    };
-
-    // bindPeer.onremovestream = function (event) {};
-
-    bindPeer.onsignalingstatechange = function (event) {
-      bindPeer.newSignalingState = bindPeer.signalingState;
-      com.onSignalingStateChange(event);
-    };
-
-    bindPeer.onicegatheringstatechange = function () {
-      com.onIceGatheringStateChange(event);
-    };
-  };
-
-  /**
-   * Handles the event when a datachannel is received.
-   * @method onDataChannel
-   * @trigger StreamJoined, mediaAccessRequired
-   * @for Connection
-   * @since 0.6.0
-   */
-  com.onDataChannel = function (event) {
-    var received = event.channel || event;
-
-    var channel = new DataChannel(received, null, function (event, data) {
-      listener(event, data);
-    });
-
-    if (channel.label === 'main') {
-      com.datachannels.main = channel;
-    } else {
-      com.datachannels.transfers[channel.label] = channel;
+      switch(data.type){
+        case com._DC_PROTOCOL_TYPE.WRQ:
+          com.WRQProtocolHandler(data);
+          break;
+        case com._DC_PROTOCOL_TYPE.ACK:
+          com.ACKProtocolHandler(data);
+          break;
+        case com._DC_PROTOCOL_TYPE.ERROR:
+          com.ERRORProtocolHandler(data);
+          break;
+        case com._DC_PROTOCOL_TYPE.CANCEL:
+          com.CANCELProtocolHandler(data);
+          break;
+        case com._DC_PROTOCOL_TYPE.MESSAGE:
+          com.MESSAGEProtocolHandler(data);
+          break;
+        default:
+          listener('datatransfer:unsupported',{
+            peerId: com.peerId,
+            data: data
+          })
+      }
     }
   };
 
-  com.RTCPeerConnection = new window.RTCPeerConnection(com.config, com.constraints);
-  
-  if (!fn.isEmpty(stream)) {
-    com.RTCPeerConnection.addStream(stream);
-  }
-  
-  if (config.dataChannel) {
-    com.datachannels.main = new DataChannel(com.RTCPeerConnection, {
-      id: com.id,
-    }, function(event, data) {
-      listener(event, data);
-    });
-  } 
 }
