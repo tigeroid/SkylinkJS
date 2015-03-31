f90284c6ba020d3750518f7ed06aee098c4cc95c
Thanh Nguyen <tienthanh8490@gmail.com>
2015-03-31 11:15:00 +0800

    ESS-124-automatedTest #comment Add rest of stream tests

--- test-karma/stream-test.js
+++ test-karma/stream-test.js
@@ -9,6 +9,321 @@ console.log('===================================================================
 
 describe("Test stream functionalities", function() {
 
+	it('joinRoom(): Test all passed bandwidth constraints', function (done) {
+	  this.timeout(15000);
+
+	  sw.leaveRoom();
+
+	  var settings = {
+	    audio: {
+	      stereo: true,
+	      mute: true
+	    },
+	    video: {
+	      resolution: {
+	        width: 1280,
+	        height: 720
+	      },
+	      frameRate: 55
+	    },
+	    bandwidth: {
+	      audio: 1234,
+	      video: 56733,
+	      data: 1232443
+	    }
+	  };
+
+	  sw.on('incomingStream', function (peerId, stream, isSelf) {
+	    if (isSelf) {
+	      assert.deepEqual([
+	        stream.getAudioTracks().length,
+	        stream.getVideoTracks().length,
+	        stream.getAudioTracks()[0].enabled,
+	        stream.getVideoTracks()[0].enabled
+	      ], [1, 1, false, true], 'Stream tracks retrieved in joinRoom is correct');
+
+	      // remove reference
+	      delete settings.audio.mute;
+
+	      assert.deepEqual(sw._streamSettings, settings,
+	        'Stream settings set in joinRoom is correct');
+
+	      // check the set stream settings
+	      assert.deepEqual(sw._getUserMediaSettings.audio, true,
+	        'Set audio in joinRoom is correct');
+	      assert.deepEqual(sw._getUserMediaSettings.video, {
+	        mandatory: {
+	          //minWidth: settings.video.resolution.width,
+	          //minHeight: settings.video.resolution.height,
+	          maxWidth: settings.video.resolution.width,
+	          maxHeight: settings.video.resolution.height,
+	          //minFrameRate: settings.video.frameRate,
+	          maxFrameRate: settings.video.frameRate
+	        },
+	        optional: []
+	      }, 'Set video in joinRoom is correct');
+
+	      // turn off all events
+	      sw.off('incomingStream');
+	      sw.off('mediaAccessError');
+
+	      done();
+	    }
+	  });
+
+	  sw.on('mediaAccessError', function (error) {
+	    assert.fail(false, true, 'Failed to get user media and parse stream settings');
+
+	    // turn off all events
+	    sw.off('incomingStream');
+	    sw.off('mediaAccessError');
+
+	    done();
+	  });
+
+	  console.log(': Test user set settings');
+	  console.log('> Requesting audio and video');
+
+	  sw.init(apikey, function(){
+	    sw.joinRoom(settings);
+	  });
+
+	  /*setTimeout(function () {
+	    // turn off all events
+	    sw.off('incomingStream');
+	    sw.off('mediaAccessError');
+
+	    t.end();
+	  }, 25000);*/
+	});
+
+	it('joinRoom(): Testing all passed media constraints', function (done) {
+
+	  this.timeout(15000);
+
+	  var audio_array = [];
+	  var video_array = [];
+
+	  sw.leaveRoom();
+
+	  sw.on('incomingStream', function (peerId, stream, isSelf) {
+	    if (isSelf) {
+	      // check stream retrieval options
+	      // check stream retrieval options
+	      if (stream.getAudioTracks().length > 0) {
+	        audio_array.push(1);
+	      }
+	      if (stream.getVideoTracks().length > 0) {
+	        video_array.push(1);
+	      }
+	      // check the set stream settings
+	      if (typeof sw._streamSettings.audio === 'object' &&
+	        sw._streamSettings.audio.stereo === false) {
+	        audio_array.push(2);
+	      }
+	      if (sw._streamSettings.video === false) {
+	        video_array.push(2);
+	      }
+	      // check the set getUserMedia settings
+	      if (sw._getUserMediaSettings.audio === true) {
+	        audio_array.push(3);
+	      }
+	      if (sw._getUserMediaSettings.video === false) {
+	        video_array.push(3);
+	      }
+
+	      assert.deepEqual(audio_array, [1, 2, 3], 'Set audio settings correct (settings=userset)');
+	      assert.deepEqual(video_array, [2, 3], 'Set video settings correct (settings=userset)');
+
+	      // turn off all events
+	      sw.off('incomingStream');
+	      sw.off('mediaAccessError');
+
+	      done();
+
+	    }
+	  });
+
+	  sw.on('mediaAccessError', function () {
+	    assert.fail(false, true, 'Failed getting user media');
+
+	    // turn off all events
+	    sw.off('incomingStream');
+	    sw.off('mediaAccessError');
+	    done();
+	  });
+
+	  console.log(': Test joinRoom with audio only');
+	  console.log('> Requesting audio only');
+
+	  sw.init(apikey,function(){
+	    sw.joinRoom({
+	      audio: true
+	    });
+	  });
+
+	  /*setTimeout(function () {
+	    // turn off all events
+	    sw.off('incomingStream');
+	    sw.off('mediaAccessError');
+
+	    done();
+	  }, 10000);*/
+	});
+
+	it('joinRoom(): Testing passed "false" constraints', function (done) {
+	  this.timeout(15000);
+
+	  sw.leaveRoom();
+
+	  var getUserMediaCalled = false;
+
+	  sw.on('mediaAccessSuccess', function () {
+	    getUserMediaCalled = true;
+	  });
+
+	  sw.on('mediaAccessError', function () {
+	    getUserMediaCalled = true;
+	  });
+
+	  console.log(': Test joinRoom with no audio and video');
+
+	  sw.init(apikey, function(){
+	    sw.joinRoom({
+	      audio: false,
+	      video: false
+	    }, function () {
+	      assert.deepEqual(getUserMediaCalled, false, 'Get user media is not called');
+	      assert.deepEqual({
+	        audio: sw._streamSettings.audio,
+	        video: sw._streamSettings.video
+	      }, {
+	        video: false,
+	        audio: false
+	      }, 'Stream settings are false');
+
+	      // turn off all events
+	      sw.off('mediaAccessSuccess');
+	      sw.off('mediaAccessError');
+	      sw.off('peerJoined');
+	      done();
+	    });
+	  });
+
+	});
+
+	// join room media constraints test
+	it('joinRoom(): Testing parsed default constraints', function(done) {
+	  this.timeout(15000);
+
+	  sw.leaveRoom();
+
+	  var getUserMediaCalled = false;
+
+	  sw.on('mediaAccessSuccess', function () {
+	    getUserMediaCalled = true;
+	  });
+
+	  sw.on('mediaAccessError', function () {
+	    getUserMediaCalled = true;
+	  });
+
+	  console.log(': Test joinRoom with no settings');
+	  sw.init(apikey, function () {
+	    sw.joinRoom(function () {
+	      assert.deepEqual(getUserMediaCalled, false, 'Get user media is not called');
+
+	      // turn off all events
+	      sw.off('mediaAccessSuccess');
+	      sw.off('mediaAccessError');
+	      done();
+	    });
+	  });
+	});
+
+	it('getUserMedia(): Test parsed video resolutions', function (done) {
+	  this.timeout(15000);
+
+	  sw.leaveRoom();
+
+	  var settings = {
+	    audio: {
+	      stereo: true,
+	      mute: false
+	    },
+	    video: {
+	      resolution: {
+	        width: 1500,
+	        height: 800
+	      },
+	      frameRate: 55,
+	      mute: true
+	    }
+	  };
+
+	  sw.on('mediaAccessSuccess', function (stream) {
+	    // remove reference
+	    delete settings.audio.mute;
+	    delete settings.video.mute;
+
+	    assert.deepEqual({
+	      audio: sw._streamSettings.audio,
+	      video: sw._streamSettings.video,
+	      mediaStatus: sw._mediaStreamsStatus
+	    }, {
+	      audio: settings.audio,
+	      video: settings.video,
+	      mediaStatus: {
+	        audioMuted: false,
+	        videoMuted: true
+	      }
+	    }, 'Stream settings set in getUserMedia is correct');
+
+	    // check stream retrieval options
+	    assert.deepEqual([
+	      stream.getAudioTracks().length,
+	      stream.getVideoTracks().length
+	    ], [1, 1], 'Stream audio and video tracks retrieved');
+
+	    // check the set stream settings
+	    assert.deepEqual(sw._getUserMediaSettings.audio, true, 'Set audio getUserMedia is correct');
+	    assert.deepEqual(sw._getUserMediaSettings.video, {
+	      mandatory: {
+	        //minWidth: settings.video.resolution.width,
+	        //minHeight: settings.video.resolution.height,
+	        maxWidth: settings.video.resolution.width,
+	        maxHeight: settings.video.resolution.height,
+	        //minFrameRate: settings.video.frameRate,
+	        maxFrameRate: settings.video.frameRate
+	      },
+	      optional: []
+	    }, 'Set video getUserMedia is correct');
+
+	    // turn off all events
+	    sw.off('mediaAccessSuccess');
+	    sw.off('mediaAccessError');
+
+	    done();
+
+	  });
+
+	  sw.on('mediaAccessError', function (error) {
+	    assert.fail(false, true, 'Failed to get user media and parse stream settings');
+
+	    // turn off all events
+	    sw.off('mediaAccessSuccess');
+	    sw.off('mediaAccessError');
+
+	    done();
+
+	  });
+
+	  console.log(': Test user set settings');
+	  console.log('> Requesting audio and video');
+
+	  sw.getUserMedia(settings);
+	});
+
 	it('getUserMedia(): Test that it should not be called if all settings is false', function (done){
 	  this.timeout(15000);
 
